aa14de0d802c152f3dce76584981ee60bbecb94d
mamuz <muzzi_is@web.de>
2014-06-07 00:05:23 +0200

    add module UT

--- src/MamuzContact/Module.php
+++ src/MamuzContact/Module.php
@@ -25,7 +25,7 @@ class Module implements
 
     public function getConfig()
     {
-        return include __DIR__ . '/../../config/module.config.php';
+        return include __DIR__ . '/../../config/module.config.php ';
     }
 
     public function getAutoloaderConfig()
